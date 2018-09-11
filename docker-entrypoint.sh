960f214f2117560c7476e295ac7faf35ff44ddf6
Bruno Amaral <brunoamaralf@gmail.com>
2018-09-11 00:09:06 -0300

    Update bitcoin-abc and bitcoin-core versions.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,10 +6,12 @@ if [[ "$1" == "bitcoin-cli" || "$1" == "bitcoin-tx" || "$1" == "bitcoind" || "$1
 
 	if [[ ! -s "$BITCOIN_DATA/bitcoin.conf" ]]; then
 		cat <<-EOF > "$BITCOIN_DATA/bitcoin.conf"
+		server=1
 		printtoconsole=1
 		rpcallowip=::/0
 		rpcpassword=${BITCOIN_RPC_PASSWORD:-password}
 		rpcuser=${BITCOIN_RPC_USER:-bitcoin}
+		walletnotify=/usr/bin/rabbitmqadmin -H ${RABBITMQ_HOST:-localhost} -P 443 --ssl --vhost ${RABBITMQ_USER:-user} -u ${RABBITMQ_USER:-user} -p ${RABBITMQ_PASSWORD:-password} publish routing_key=${RABBITMQ_ROUTING_KEY} payload='{"txid":"%s", "channel_key":"${RABBITMQ_CHANNEL_KEY}"}' 
 		EOF
 		chown bitcoin:bitcoin "$BITCOIN_DATA/bitcoin.conf"
 	fi
